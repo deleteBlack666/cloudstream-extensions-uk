26483c589f6d1103031a5bd97d0cc19e8d417c6b
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-05-04 13:46:50 +0300

    Update FundubModel.kt

--- AnimeONProvider/src/main/kotlin/com/lagradost/models/FundubModel.kt
+++ AnimeONProvider/src/main/kotlin/com/lagradost/models/FundubModel.kt
@@ -3,29 +3,20 @@ package com.lagradost.models
 import com.google.gson.annotations.SerializedName
 
 data class FundubsModel (
-        @SerializedName("funDubs") val fundubs : List<FundubModel>,
+    @SerializedName("funDubs") val fundubs : List<FundubModel>
 )
 
 data class FundubModel (
-
-        @SerializedName("fundub") val fundub : Fundub,
-        @SerializedName("player") val player : List<FundubPlayer>
+    @SerializedName("fundub") val fundub : Fundub,
+    @SerializedName("player") val player : List<FundubPlayer>
 )
 
 data class Fundub (
-
-        @SerializedName("id") val id : Int,
-        @SerializedName("name") val name : String,
-        // @SerializedName("synonyms") val synonyms : List<String>,
-        // @SerializedName("description") val description : String,
-        // @SerializedName("team") val team : String,
-        // @SerializedName("telegram") val telegram : String,
-        // @SerializedName("youtube") val youtube : String,
-        // @SerializedName("avatar") val avatar : String
+    @SerializedName("id") val id : Int,
+    @SerializedName("name") val name : String
 )
 
 data class FundubPlayer (
-
-        @SerializedName("name") val name : String,
-        @SerializedName("id") val id : Int
+    @SerializedName("name") val name : String,
+    @SerializedName("id") val id : Int
 )
