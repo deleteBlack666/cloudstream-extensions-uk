b1f9cc973f39e6960f5a1f39321dfb2b1a0ee055
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-05-04 10:46:26 +0300

    fixmaybe build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,13 +27,12 @@ subprojects {
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
         // you can modify it to use other git hosting services, like gitlab
-        // setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/CakesTwix/cloudstream-extensions-uk")
         setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/deleteBlack666/cloudstream-extensions-uk")
 authors = listOf("deleteBlack666")
     }
 
     android {
-        namespace = "ua.CakesTwix"
+        namespace = "ua.deleteBlack666"
 
         defaultConfig {
             minSdk = 21
