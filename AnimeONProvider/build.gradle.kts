7aa383fb1ee1a8c56920706737b12659dfec2672
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-05-07 19:42:36 +0300

    Build 093d8ef15ccba7f57fe54d626e283bc923c88aa7

---
+++ AnimeONProvider/build.gradle.kts
@@ -0,0 +1,33 @@
+// use an integer for version numbers
+version = 9
+
+dependencies {
+    implementation(libs.gson)
+
+    testImplementation(libs.junit)
+    testImplementation(libs.kotlinx.coroutines.test)
+}
+
+cloudstream {
+    language = "uk"
+    // All of these properties are optional, you can safely remove them
+
+    description = "AnimeON - Дивитися аніме україньскою"
+    authors = listOf("CakesTwix")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "Anime",
+        "AnimeMovie",
+        "OVA",
+    )
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=animeon.club&sz=%size%"
+}
