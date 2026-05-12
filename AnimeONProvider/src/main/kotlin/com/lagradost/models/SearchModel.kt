364248dc31144b007dc787b73a91a5da01fc92a3
deleteBlack666 <78680082+deleteBlack666@users.noreply.github.com>
2026-05-12 17:22:43 +0300

    Update SearchModel.kt

--- AnimeONProvider/src/main/kotlin/com/lagradost/models/SearchModel.kt
+++ AnimeONProvider/src/main/kotlin/com/lagradost/models/SearchModel.kt
@@ -3,16 +3,14 @@ package com.lagradost.models
 import com.google.gson.annotations.SerializedName
 
 data class SearchModel (
-    @SerializedName("results") val results : List<Results>,
-    @SerializedName("totalCount") val totalCount : Int? = null
+    @SerializedName("results") val results : List<AnimeSearchResult>,
 )
 
-data class Results (
+data class AnimeSearchResult (
     @SerializedName("titleUa") val titleUa : String,
     @SerializedName("id") val id : Int,
     @SerializedName("image") val image : Image,
     @SerializedName("episodes") val episodes : Int? = null,
     @SerializedName("episodesAired") val episodesAired : Int? = null,
-    @SerializedName("type") val type : String? = null, // Важливо для фільмів/серіалів
-    @SerializedName("status") val status : String? = null
+    @SerializedName("type") val type : String? = null
 )
